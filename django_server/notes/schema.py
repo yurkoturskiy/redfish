8d9a3f3c5efab960977738d8c80e19d699730a49
Guandjoy <guandjoy@gmail.com>
2019-01-28 21:35:24 +0200

    Add user model/profile into Graphene schema

--- django_server/notes/schema.py
+++ django_server/notes/schema.py
@@ -4,6 +4,7 @@ from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import Note
+from django.contrib.auth.models import User
 
 
 # Graphene will automatically map the Category model's fields onto the CategoryNode.
@@ -14,10 +15,18 @@ class NoteNode(DjangoObjectType):
         filter_fields = ['title', 'content', 'color', 'pinned']
         interfaces = (relay.Node, )
 
+class UserNode(DjangoObjectType):
+    class Meta:
+        model = User
+        filter_fields = ['username']
+        exclude_fields = ('password', 'is_superuser', 'is_staff',)
+        interfaces = (relay.Node, )
 
 class Query(object):
     note = relay.Node.Field(NoteNode)
     all_notes = DjangoFilterConnectionField(NoteNode)
+    # user = relay.Node.Field(UserNode)
+    profile = DjangoFilterConnectionField(UserNode)
 
     def resolve_all_notes(self, info):
         # context will reference to the Django request
@@ -25,3 +34,9 @@ class Query(object):
             return Note.objects.none()
         else:
             return Note.objects.filter(owner=info.context.user)
+
+    def resolve_profile(self, info):
+        print(info.context.user)
+        if info.context.user.is_authenticated:
+            return User.objects.filter(username=info.context.user)
+
