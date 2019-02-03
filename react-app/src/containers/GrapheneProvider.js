56280dd3227d2bc0bf53dc540ad62abfe4bb64bb
Guandjoy <guandjoy@gmail.com>
2019-02-03 22:24:24 +0200

    Init Apollo client state

--- react-app/src/containers/GrapheneProvider.js
+++ react-app/src/containers/GrapheneProvider.js
@@ -3,8 +3,25 @@ import { connect } from 'react-redux'
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link'
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { withClientState } from 'apollo-link-state'
+
+const cache = new InMemoryCache()
+
+const defaultState = {
+  applicationState: {
+    __typename: 'applicationState',
+    isAuth: localStorage.getItem('token') ? 'true' : 'false',
+  }
+}
+
+const stateLink = withClientState({
+  cache,
+  defaults: defaultState,
+})
+
 
 const httpLink = createHttpLink({
   uri: "http://localhost:9000/graphql",
@@ -23,8 +40,11 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
+  link: ApolloLink.from([
+    stateLink,
+    authLink.concat(httpLink),
+  ]),
+  cache,
 });
 
 class GrapheneProvider extends React.Component {
