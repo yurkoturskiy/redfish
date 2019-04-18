fc35e3fac0524e8baaab3c1ec122daeaa80977a3
USER-PC\Admin <guandjoy@gmail.com>
2019-04-18 20:44:02 +0300

    Fix css options

--- react-app/src/containers/application/Logo.js
+++ react-app/src/containers/application/Logo.js
@@ -7,8 +7,8 @@ export const LogoStyledImg = styled.img`
   top: 32px;
   left: 32px;
   width: 38px;
-  boxShadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-  borderRadius: 4px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  border-radius: 4px;
 `
 
 function Logo() {
