07aca50ec4be897d56815202c216c1895abdb1fa
USER-PC\Admin <guandjoy@gmail.com>
2019-04-18 20:34:48 +0300

    Init logo component

--- react-app/src/containers/application/Application.js
+++ react-app/src/containers/application/Application.js
@@ -1,12 +1,14 @@
 import React from "react";
 // components
 import NotesContainer from "./notes/NotesContainer"
+import Logo from "./Logo"
 
 
 class Application extends React.Component {
   render() {
     return (
       <div>
+        <Logo/>
         <NotesContainer/>
       </div>
     );
---
+++ react-app/src/containers/application/Logo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import icon from '../../static/icon.svg'
+
+export const LogoStyledImg = styled.img`
+  position: absolute;
+  top: 32px;
+  left: 32px;
+  width: 38px;
+  boxShadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  borderRadius: 4px;
+`
+
+function Logo() {
+  return (
+    <LogoStyledImg src={icon} />
+  )
+}
+
+export default Logo
