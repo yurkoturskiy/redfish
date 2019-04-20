7b23634a44b90ff1dfdc34cd42fd343f84c5c406
USER-PC\Admin <guandjoy@gmail.com>
2019-04-20 09:14:35 +0300

    Add remove_order_gaps and fill_gaps managers

--- django_server/notes/managers.py
+++ django_server/notes/managers.py
@@ -14,6 +14,27 @@ class NoteManager(models.Manager):
                 note.order = index + 1
                 note.save()
 
+    def remove_order_gaps(self):
+        """ Remove all gaps in ordering which somehow occured
+        Remove the root cause of this issue """
+        users = User.objects.all()
+        for user in users:
+            qs = self.get_queryset().filter(owner=user).order_by('order')
+            for index, note in enumerate(qs):
+                print(f'Old order: {note.order}. New order: {index + 1}')
+                note.order = index + 1
+                note.save()
+
+
+    def fill_gaps(self, objects):
+        """ Pass notes here before delete them """
+        qs = self.get_queryset()
+        
+        with transaction.atomic():
+            for obj in objects:
+                qs.filter(order__gt=obj.order, owner=obj.owner).update(order=F('order') - 1)
+
+
     def move(self, obj, new_order):
         """ Move an object to a new order position """
 
@@ -43,6 +64,7 @@ class NoteManager(models.Manager):
 
             obj.order = new_order
             obj.save()
+
 
     def create(self, **kwargs):
         instance = self.model(**kwargs)
