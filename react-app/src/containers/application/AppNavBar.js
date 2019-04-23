c538094895edba35a7e29332b0eb407022d1ab01
Guandjoy <guandjoy@gmail.com>
2019-04-23 12:00:42 +0300

    Rebuild navigation

--- react-app/src/containers/application/AppNavBar.js
+++ react-app/src/containers/application/AppNavBar.js
@@ -1,55 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 import gql from 'graphql-tag'
 import { withRouter } from 'react-router'
 import { withApollo } from 'react-apollo'
-import { 
-  Navbar,
-  Nav,
-  NavDropdown,
-  Button,
-} from 'react-bootstrap'
-import icon from '../../static/icon.svg'
+import Logo from './Logo'
+import PreferencesBtn from './preferences/PreferencesBtn'
 
-const logoutQuery = gql`
-  query {
-    logout(input: {}) @rest(type: "Logout", method: "POST", path: "rest-auth/logout/") {
-      __typename
-    }
-  }
+export const Wrapper = styled.div`
+  height: 128px;
 `
 
 function NavBar(props) {
-  const logout = () => {
-    props.client.query({query: logoutQuery})
-    localStorage.removeItem('token')
-    props.client.writeData({ data: { isAuth: false } })
-    props.history.push('/login')
-    console.log('logout')
-  }
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand onClick={() => props.history.push('/app')}>
-          <img src={icon} style={{ 
-            width: '38px',
-            boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
-            borderRadius: '4px'
-          }}/>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse className="justify-content-end">
-        <Nav>
-          <NavDropdown alignRight title="Username" id="nav-dropdown">
-            <NavDropdown.Item onClick={() => props.history.push('/profile') }>
-              Profile
-            </NavDropdown.Item>
-            <NavDropdown.Item onClick={logout}>
-              Logout
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <Wrapper>
+      <Logo />
+      <PreferencesBtn />
+    </Wrapper>
   )
 }
 
-export default withApollo(withRouter(NavBar))
+export default withRouter(NavBar)
--- react-app/src/containers/application/Application.js
+++ react-app/src/containers/application/Application.js
@@ -2,16 +2,14 @@ import React from "react";
 // components
 import NotesContainer from "./notes/NotesContainer"
 import Logo from "./Logo"
-import PreferencesBtn from "./PreferencesBtn"
+import PreferencesBtn from "./preferences/PreferencesBtn"
 
 
 class Application extends React.Component {
   render() {
     return (
       <div>
-        <Logo/>
         <NotesContainer/>
-        <PreferencesBtn/>
       </div>
     );
   }
