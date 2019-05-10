d4ac0494d8bd8defcb5ab1d02823c3cbfad2ea48
Guandjoy <guandjoy@gmail.com>
2019-05-10 22:14:34 +0300

    Refactor SelectedNotesOptionBar

--- react-app/src/containers/application/notes/Notes.js
+++ react-app/src/containers/application/notes/Notes.js
@@ -1,64 +1,70 @@
 // external
-import React, { useState, useEffect } from 'react'
-import { withApollo, Query, graphql } from "react-apollo"
+import React, { useState, useEffect } from "react";
+import { withApollo, Query, graphql } from "react-apollo";
 // local components
-import Note from './note/Note'
-import DraggableMasonryLayout from './DraggableMasonryLayout'
-import SelectedNotesOptionsBar from './SelectedNotesOptionsBar'
+import Note from "./note/Note";
+import DraggableMasonryLayout from "./DraggableMasonryLayout";
+import SelectedNotesOptionsBar from "./SelectedNotesOptionsBar";
 // queries
-import { ALL_NOTES, SELECTED_NOTES } from './queries'
+import { ALL_NOTES, SELECTED_NOTES } from "./queries";
 
 function Notes(props) {
-  const selectionHandler = () => {
-
-  }
+  const selectionHandler = () => {};
   return (
-    <Query query={ALL_NOTES} >
+    <Query query={ALL_NOTES}>
       {({ loading, error, data, fetchMore }) => {
-        if (loading) return <p>Loading...</p>
+        if (loading) return <p>Loading...</p>;
         if (error) {
-          console.log(error)
-          return <p>Error :(</p>
+          console.log(error);
+          return <p>Error :(</p>;
         }
         const cards = data.allNotes.edges.map((note, index) => (
-          <Note 
-            key={note.node.id} 
-            node={note.node} 
-            number={index + 1} 
-            selectionHandler={selectionHandler} 
+          <Note
+            key={note.node.id}
+            node={note.node}
+            number={index + 1}
+            selectionHandler={selectionHandler}
             isSelected={props.selectedNotes.indexOf(note.node.id) !== -1}
           />
-        ))
+        ));
         return (
           <React.Fragment>
-            <SelectedNotesOptionsBar/>
-            <DraggableMasonryLayout onEndlineEnter={() => {
-              if (data.allNotes.pageInfo.hasNextPage) {
-                fetchMore({
-                query: ALL_NOTES,
-                variables: {
-                  cursor: data.allNotes.pageInfo.endCursor
-                },
-                updateQuery: (prevResult, { fetchMoreResult }) => {
-                  if (!fetchMoreResult) return prevResult
-                  let mix = prevResult
-                  mix.allNotes.pageInfo = fetchMoreResult.allNotes.pageInfo
-                  mix.allNotes.edges = [...prevResult.allNotes.edges, ...fetchMoreResult.allNotes.edges]
-                  return mix
-                }              
-              })
-            }}}>
+            {props.selectedNotes.length > 0 && (
+              <SelectedNotesOptionsBar selectedNotes={props.selectedNotes} />
+            )}
+            <DraggableMasonryLayout
+              onEndlineEnter={() => {
+                if (data.allNotes.pageInfo.hasNextPage) {
+                  fetchMore({
+                    query: ALL_NOTES,
+                    variables: {
+                      cursor: data.allNotes.pageInfo.endCursor
+                    },
+                    updateQuery: (prevResult, { fetchMoreResult }) => {
+                      if (!fetchMoreResult) return prevResult;
+                      let mix = prevResult;
+                      mix.allNotes.pageInfo = fetchMoreResult.allNotes.pageInfo;
+                      mix.allNotes.edges = [
+                        ...prevResult.allNotes.edges,
+                        ...fetchMoreResult.allNotes.edges
+                      ];
+                      return mix;
+                    }
+                  });
+                }
+              }}
+            >
               {cards}
             </DraggableMasonryLayout>
           </React.Fragment>
-        )
+        );
       }}
     </Query>
-  )
+  );
 }
 
 export default graphql(SELECTED_NOTES, {
   props: ({ data: { selectedNotes } }) => ({
     selectedNotes
   })
-})(Notes)
+})(Notes);
--- react-app/src/containers/application/notes/SelectedNotesOptionsBar.js
+++ react-app/src/containers/application/notes/SelectedNotesOptionsBar.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import { css } from 'linaria'
-import { Query } from 'react-apollo'
-import gql from 'graphql-tag'
+import React, { useState } from "react";
+import { css } from "linaria";
+import { Query } from "react-apollo";
 
 const barWrapper = css`
   position: fixed;
-  display: var(--selected-notes-options-bar-wrapper-display);
   top: 0;
   left: 50%;
   z-index: 4;
@@ -13,31 +11,14 @@ const barWrapper = css`
   height: 48px;
   margin-left: -250px;
   background-color: lightgrey;
-`
-const query = gql`
-  query {
-    selectedNotes @client
-  }
-`
+`;
+
 function SelectedNotesOptionsBar(props) {
   return (
-    <Query query={query}>
-    {({ loading, error, data, fetchMore }) => {
-      if (loading) return <p>Loading...</p>
-      if (error) return <p>Error</p>
-      const numNotesSelected = data.selectedNotes.length
-      return (
-        <div 
-          className={barWrapper}
-          style={{'--selected-notes-options-bar-wrapper-display': numNotesSelected ? 'block' : 'none'}}
-        >
-          <p>items selected: {numNotesSelected}</p>
-        </div>
-      )
-    }}
-
-    </Query>
-  )
+    <div className={barWrapper}>
+      <p>items selected: {props.selectedNotes.length}</p>
+    </div>
+  );
 }
 
-export default SelectedNotesOptionsBar
+export default SelectedNotesOptionsBar;
