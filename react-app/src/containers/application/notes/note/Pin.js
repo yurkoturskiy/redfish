d47b6e0807c88f84ae5036b3a46139c335d0e186
Guandjoy <guandjoy@gmail.com>
2019-05-26 19:35:06 +0300

    Add comments for cache update function

--- react-app/src/containers/application/notes/note/Pin.js
+++ react-app/src/containers/application/notes/note/Pin.js
@@ -6,34 +6,40 @@ import { ALL_NOTES, SWITCH_PIN_NOTES } from "../queries";
 import { NoteNode } from "./Note";
 import { Cursors } from "../Notes";
 
-function Pin(props) {
+function Pin() {
+  /* Icon for pin/unpin action */
   const node = useContext(NoteNode);
   const cursors = useContext(Cursors);
   const updateCache = (cache, { data: { switchPinNotes } }) => {
     const cacheData = cache.readQuery({ query: ALL_NOTES });
     const {
-      action,
-      prevPinnedStatus,
-      curPinnedStatus,
-      prevOrder,
-      curOrder
+      action, // "pin" or "unpin"
+      prevPinnedStatus, // [true, false]
+      curPinnedStatus, // [false, false]
+      prevOrder, // [2, 9]
+      curOrder // [4, 5]
     } = switchPinNotes;
     cacheData.allNotes.edges.map(edge => {
+      // Iterate through all notes in cache
       prevPinnedStatus.forEach((prevPinnedSts, index) => {
+        // Get index and iterate through received status arrays
         if (
           edge.node.pinned === prevPinnedStatus[index] &&
           edge.node.order === prevOrder[index]
         ) {
+          // Update note with changed pinned status
           edge.node.pinned = curPinnedStatus[index];
           edge.node.order = curOrder[index];
-          return edge;
+          return edge; // Prevent further ordering changes
         }
         if (action === "unpin") {
+          // Update ordering on unpinning
           if (!edge.node.pinned) edge.node.order += 1;
           if (edge.node.pinned && edge.node.order > prevOrder[index])
             edge.node.order -= 1;
         }
         if (action === "pin") {
+          // Update ordering on pinning
           if (!edge.node.pinned && edge.node.order > prevOrder[index])
             edge.node.order -= 1;
         }
