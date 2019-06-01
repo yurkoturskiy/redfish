510b55486ad8cba0755b7fb3baf2673126a3e817
Guandjoy <guandjoy@gmail.com>
2019-06-01 18:42:02 +0300

    Cleanup

--- react-app/src/containers/NavigationContainer.js
+++ react-app/src/containers/NavigationContainer.js
@@ -1,46 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
-import { withApollo, compose, graphql } from "react-apollo";
-import gql from "graphql-tag";
+import { withApollo } from "react-apollo";
 // components
-import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
-import LandingNavBar from "./landing/LandingNavBar";
 import AppNavBar from "./application/AppNavBar";
 // graphql
-import icon from "../static/icon.svg";
-import appState from "../graphql/appState";
 import logout from "../graphql/logout";
 
-class NavigationContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.logout = this.logout.bind(this);
-  }
-  logout() {
-    this.props.client.query({ query: logout });
+function NavigationContainer(props) {
+  const logout = () => {
+    props.client.query({ query: logout });
     localStorage.removeItem("token");
-    this.props.client.cache.reset();
-    this.props.history.push("/login");
+    props.client.cache.reset();
+    props.history.push("/login");
     console.log("logout");
-  }
-  render() {
-    if (this.props.isAuth) {
-      return <AppNavBar />;
-    } else {
-      return <LandingNavBar />;
-    }
-  }
+  };
+  return <AppNavBar />;
 }
 
-export default withApollo(
-  withRouter(
-    compose(
-      graphql(appState, {
-        props: ({ data: { isAuth } }) => ({
-          isAuth
-        })
-      })
-    )(NavigationContainer)
-  )
-);
+export default withApollo(withRouter(NavigationContainer));
