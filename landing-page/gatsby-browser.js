d21b34b8e7d0b9d251328d24eedd9c9d5fa22138
Guandjoy <guandjoy@gmail.com>
2019-06-03 20:30:46 +0300

    Cleanup

--- landing-page/gatsby-browser.js
+++ landing-page/gatsby-browser.js
@@ -9,10 +9,8 @@ import React, { useState, useEffect } from 'react'
 import Authentication from './src/components/authentication'
 
 export const wrapRootElement = ({ element }) => {
-  console.log('search iframe', window.location.pathname.indexOf('iframe'))
   if (window.location.pathname.indexOf('iframe') === -1) {
     // Behave as usual if pathname is not for iframe
-    console.log('authentication')
     return <Authentication>{element}</Authentication>
   } else {
     return <React.Fragment>{element}</React.Fragment>
