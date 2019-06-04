4d92fe0384678e1be39c2951771eeeba887f5d2c
guandjoy <guandjoy@gmail.com>
2019-06-05 01:52:11 +0300

    Refactor with GraphQL

--- landing-page/src/components/forms/registration/RegistrationFormContainer.js
+++ landing-page/src/components/forms/registration/RegistrationFormContainer.js
@@ -1,29 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik } from 'formik'
+import { useMutation } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 import zxcvbn from 'zxcvbn'
-import FormsMaster from '../FormsMaster'
+// Components
 import RegistrationForm from './RegistrationForm'
 
-class RegistrationFormContainer extends FormsMaster {
-  constructor(props) {
-    super(props)
-    this.endpoint = 'rest-auth/registration/'
-    this.passwordStrengthValidation = this.passwordStrengthValidation.bind(this)
-    this.handleResponse = this.handleResponse.bind(this)
-    this.prepareValues = this.prepareValues.bind(this)
-    this.state = {
-      requestIsSucceed: false,
-      passwordStrengthScore: undefined,
+const SUBMIT_REGISTRATION = gql`
+  mutation registration(
+    $username: String = ""
+    $email: String = ""
+    $password1: String = ""
+  ) {
+    registration(
+      input: {
+        username: $username
+        email: $email
+        password1: $password1
+        password2: $password1
+      }
+    ) {
+      key
     }
   }
-  prepareValues(values) {
-    values['password2'] = values.password1
-    return values
+`
+
+function RegistrationFormContainer(props) {
+  const [submitRegistration] = useMutation(SUBMIT_REGISTRATION)
+  const [requestIsSucceed, setRequestIsSucceed] = useState(false)
+  const [passwordStrengthScore, setPasswordStrengthScore] = useState(undefined)
+
+  const handleSubmit = (values, { setSubmitting, setErrors, setStatus }) => {
+    submitRegistration({ variables: values })
+      .then(response => {
+        setRequestIsSucceed(true)
+        setSubmitting(false)
+      })
+      .catch(error => {
+        if (error.graphQLErrors[0]) {
+          let error_message = JSON.parse(error.graphQLErrors[0].message)
+          setErrors(error_message)
+          setStatus({ non_field_errors: error_message.non_field_errors })
+        } else if (!error.graphQLErrors[0]) {
+          setStatus({ non_field_errors: 'Something wrong with a server' })
+          console.log('Something wrong with a server')
+        } else {
+          console.log('Error', error.graphQLErrors)
+        }
+        setSubmitting(false)
+      })
   }
-  handleResponse(response) {
-    this.setState({ requestIsSucceed: true })
-  }
-  passwordStrengthValidation(payload) {
+
+  const passwordStrengthValidation = payload => {
     let fieldIsEmpty = payload.target.value.length === 0 ? true : false
     let evaluate = zxcvbn(payload.target.value)
     let helperText =
@@ -32,37 +60,33 @@ class RegistrationFormContainer extends FormsMaster {
       (evaluate.score === 2 && 'somewhat guessable') ||
       (evaluate.score === 3 && 'safely unguessable') ||
       (evaluate.score === 4 && 'very unguessable')
-    this.setState({
-      passwordStrengthScore: fieldIsEmpty ? undefined : helperText,
-    })
+    setPasswordStrengthScore(fieldIsEmpty ? undefined : helperText)
   }
-  render() {
-    if (this.state.requestIsSucceed) {
-      return <h1>Confirm your email address</h1>
-    } else {
-      return (
-        <Formik
-          initialValues={{
-            username: undefined,
-            email: undefined,
-            password1: undefined,
-          }}
-          onSubmit={this.handleSubmit}
-        >
-          {({ status, touched, isSubmitting, errors, handleChange }) => (
-            <RegistrationForm
-              status={status}
-              touched={touched}
-              isSubmitting={isSubmitting}
-              errors={errors}
-              onChange={handleChange}
-              passwordOnChange={this.passwordStrengthValidation}
-              passwordHelperText={this.state.passwordStrengthScore}
-            />
-          )}
-        </Formik>
-      )
-    }
+  if (requestIsSucceed) {
+    return <h1>Confirm your email address</h1>
+  } else {
+    return (
+      <Formik
+        initialValues={{
+          username: undefined,
+          email: undefined,
+          password1: undefined,
+        }}
+        onSubmit={handleSubmit}
+      >
+        {({ status, touched, isSubmitting, errors, handleChange }) => (
+          <RegistrationForm
+            status={status}
+            touched={touched}
+            isSubmitting={isSubmitting}
+            errors={errors}
+            onChange={handleChange}
+            passwordOnChange={passwordStrengthValidation}
+            passwordHelperText={passwordStrengthScore}
+          />
+        )}
+      </Formik>
+    )
   }
 }
 
