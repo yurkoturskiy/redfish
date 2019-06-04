4545a26b1ea72c3ad59a49ef579d26fcb54670aa
guandjoy <guandjoy@gmail.com>
2019-06-05 02:15:31 +0300

    Refactor with GraphQL

--- landing-page/src/components/forms/passwordReset/PasswordResetFormContainer.js
+++ landing-page/src/components/forms/passwordReset/PasswordResetFormContainer.js
@@ -1,41 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useMutation } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 import { Formik } from 'formik'
-
-import FormsMaster from '../FormsMaster'
+// Components
 import PasswordResetForm from './PasswordResetForm'
 
-class PasswordResetFormContainer extends FormsMaster {
-  constructor(props) {
-    super(props)
-    this.endpoint = 'rest-auth/password/reset/'
-    this.handleResponse = this.handleResponse.bind(this)
-    this.state = {
-      requestIsSucceed: false,
+const PASSWORD_RESET = gql`
+  mutation passwordReset($email: String = "") {
+    passwordReset(input: { email: $email }) {
+      detail
     }
   }
-  handleResponse(response) {
-    this.setState({ requestIsSucceed: true })
+`
+
+function PasswordResetFormContainer() {
+  const [passwordReset] = useMutation(PASSWORD_RESET)
+  const [requestIsSucceed, setRequestIsSucceed] = useState(false)
+
+  const handleSubmit = (values, { setSubmitting, setErrors, setStatus }) => {
+    passwordReset({ variables: values })
+      .then(response => {
+        handleResponse()
+        setSubmitting(false)
+      })
+      .catch(error => {
+        if (error.graphQLErrors[0]) {
+          let error_message = JSON.parse(error.graphQLErrors[0].message)
+          setErrors(error_message)
+          setStatus({ non_field_errors: error_message.non_field_errors })
+        } else if (!error.graphQLErrors[0]) {
+          setStatus({ non_field_errors: 'Something wrong with a server' })
+          console.log('Something wrong with a server')
+        } else {
+          console.log('Error', error.graphQLErrors)
+        }
+        setSubmitting(false)
+      })
   }
-  render() {
-    if (this.state.requestIsSucceed) {
-      return <h3>Check your email</h3>
-    } else {
-      return (
-        <Formik
-          initialValues={{ email: undefined }}
-          onSubmit={this.handleSubmit}
-        >
-          {({ status, touched, isSubmitting, errors }) => (
-            <PasswordResetForm
-              status={status}
-              touched={touched}
-              isSubmitting={isSubmitting}
-              errors={errors}
-            />
-          )}
-        </Formik>
-      )
-    }
+
+  const handleResponse = () => {
+    setRequestIsSucceed(true)
+  }
+
+  if (requestIsSucceed) {
+    return <h3>Check your email</h3>
+  } else {
+    return (
+      <Formik initialValues={{ email: undefined }} onSubmit={handleSubmit}>
+        {({ status, touched, isSubmitting, errors }) => (
+          <PasswordResetForm
+            status={status}
+            touched={touched}
+            isSubmitting={isSubmitting}
+            errors={errors}
+          />
+        )}
+      </Formik>
+    )
   }
 }
 
