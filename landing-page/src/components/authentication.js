8410cd5ec05be134d3ced107d11cdf084b75dbc2
guandjoy <guandjoy@gmail.com>
2019-06-05 15:48:56 +0300

    Refactor

--- landing-page/src/components/authentication.js
+++ landing-page/src/components/authentication.js
@@ -1,16 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+
+const query = gql`
+  query {
+    tokenIsValid
+  }
+`
 
 function Authentication(props) {
-  const [key, setKey] = useState(localStorage.getItem('token'))
-  const [notAuthenticated, setNotAuthenticated] = useState(false)
+  const { loading, data } = useQuery(query)
   useEffect(() => {
-    if (key) window.location.replace('http://localhost:3006/')
-    if (!key) setNotAuthenticated(true)
-  }, [])
-  if (notAuthenticated) {
-    return <React.Fragment>{props.children}</React.Fragment>
-  }
-  return null
+    if (data.tokenIsValid) window.location.replace(process.env.REDFISH_APP_URL)
+    else localStorage.removeItem('token')
+  })
+  if (loading) return <div>loading...</div>
+  if (data.tokenIsValid) return null
+  return <React.Fragment>{props.children}</React.Fragment>
 }
 
 export default Authentication
