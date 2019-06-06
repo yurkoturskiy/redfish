a8caf9d1e53bbe5a9864a4b798a1a70d50de2aa3
guandjoy <guandjoy@gmail.com>
2019-06-06 19:26:00 +0300

    Setup GitHub OAuth

--- landing-page/src/components/forms/AuthWithGitHub.js
+++ landing-page/src/components/forms/AuthWithGitHub.js
@@ -1,17 +1,77 @@
-import React from 'react'
-import { css } from 'linaria'
+// import React from 'react'
+// import { css } from 'linaria'
 
+// import GitHubLogin from 'react-github-login'
+
+// function AuthWithGitHub(props) {
+//   const onSuccess = response => console.log(response)
+//   const onFailure = response => console.log(response)
+
+//   return (
+//     <div style={{ position: 'absolute', top: '200px' }}>
+//       <GitHubLogin
+//         clientId="6aee15cdc641688b6f3e"
+//         onSuccess={onSuccess}
+//         onFailure={onFailure}
+//         redirectUri=""
+//       />
+//     </div>
+//   )
+// }
+
+// export default AuthWithGitHub
+
+import React, { useState, useEffect } from 'react'
+import { useMutation } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 import GitHubLogin from 'react-github-login'
 
+const AUTH_WITH_GITHUB = gql`
+  mutation authWithGithub($code: String!) {
+    authWithGithub(input: { code: $code }) {
+      key
+    }
+  }
+`
+
 function AuthWithGitHub(props) {
-  const onSuccess = response => console.log(response)
-  const onFailure = response => console.log(response)
+  const [isAuth, setIsAuth] = useState(false)
+  const [authWithGitHub, { error, data }] = useMutation(AUTH_WITH_GITHUB)
+  const [code, setCode] = useState(undefined)
+
+  useEffect(() => {
+    if (code) {
+      authWithGitHub({ variables: { code } }).then(response => {
+        handleResponse(response)
+        console.log(response)
+      })
+    }
+    if (isAuth) {
+      window.location.replace(process.env.REDFISH_APP_URL)
+    }
+  }, [code, isAuth])
+
+  const handleResponse = response => {
+    localStorage.setItem('token', response.data.authWithGithub.key)
+    setIsAuth(true)
+    console.log('Token received and saved')
+  }
+
+  const responseGitHub = response => {
+    console.log('GitHub response', response)
+    setCode(response.code)
+  }
+
+  const onFailure = response => {
+    console.log('GitHub failure response', response)
+  }
 
   return (
-    <div style={{ position: 'absolute', top: '200px' }}>
+    <div>
+      {' '}
       <GitHubLogin
         clientId="6aee15cdc641688b6f3e"
-        onSuccess={onSuccess}
+        onSuccess={responseGitHub}
         onFailure={onFailure}
         redirectUri=""
       />
