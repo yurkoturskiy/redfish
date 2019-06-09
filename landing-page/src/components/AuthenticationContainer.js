b5b8f19081869591faa269a6be275f4abb1be010
guandjoy <guandjoy@gmail.com>
2019-06-09 13:50:10 +0300

    Add OAuth buttons

--- landing-page/src/components/AuthenticationContainer.js
+++ landing-page/src/components/AuthenticationContainer.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { css } from 'linaria'
+// Local components
+import AuthWithFacebook from './authentication/AuthWithFacebook'
+import AuthWithGitHub from './authentication/AuthWithGitHub'
 
 const wrapper = css`
   width: 300px;
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%);
   padding: 24px;
-  boder: 1px solid lightgrey;
+  border: 1px solid lightgrey;
 `
 
 function AuthenticationContainer(props) {
-  return <div className={wrapper}>Authentication container</div>
+  return (
+    <div className={wrapper}>
+      <AuthWithFacebook>Continue with Facebook</AuthWithFacebook>
+      <AuthWithGitHub>Continue with GitHub</AuthWithGitHub>
+    </div>
+  )
 }
 
 export default AuthenticationContainer
