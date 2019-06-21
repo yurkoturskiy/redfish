ed56766aec062ccd4861111a59ce3f7b5ae67a36
guandjoy <guandjoy@gmail.com>
2019-06-22 00:23:38 +0300

    Retreive key from localStorage after component is mount

--- landing-page/src/pages/iframe-key.js
+++ landing-page/src/pages/iframe-key.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 
 function KeyToApp(props) {
-  const [key] = useState(localStorage.getItem('token'))
   useEffect(() => {
+    const key = localStorage.getItem('token')
     window.parent.postMessage(JSON.stringify({ key }), 'http://localhost:3006/')
   }, [])
   return <div>Landing's iframe for retrieving key from it's localStorage</div>
