66575f587f0a61a63db13e72c118acbaff5a5d96
guandjoy <guandjoy@gmail.com>
2019-06-27 01:15:44 +0300

    Add adaptivity

--- landing-page/src/components/hero/content/Header.js
+++ landing-page/src/components/hero/content/Header.js
@@ -2,16 +2,68 @@ import React from 'react'
 import { css } from 'linaria' // eslint-disable-line
 
 const header = css`
+  margin: 0;
+  margin-left: var(--gutter);
+  margin-right: var(--gutter);
+  color: #444444;
+
+  /*******************/
+  /* Adapt text size */
+  /*******************/
+
   font-style: normal;
   font-weight: bold;
   font-size: 3rem;
   line-height: 56px;
-  margin: 0 0 0 12px;
-  color: #444444;
+
+  @media screen and (max-width: 800px) and (min-width: 415px) {
+    font-size: 40px;
+    line-height: 47px;
+  }
+
+  @media screen and (max-width: 414px) and (min-width: 321px) {
+    font-weight: 500;
+    font-size: 28px;
+    line-height: 33px;
+  }
+
+  @media screen and (max-width: 320px) {
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 28px;
+  }
+
+  /*********************/
+  /* Adapt text-alight */
+  /*********************/
+
+  @media screen and (max-width: 540px) {
+    text-align: center;
+  }
+
+  /************************/
+  /* Handle line breaking */
+  /************************/
+
+  br {
+    display: none;
+  }
+
+  @media screen and (max-width: 1024px) {
+    br {
+      display: block;
+    }
+  }
 `
 
 function Header(props) {
-  return <h1 className={header}>Fullstack Boilerplate Web Application</h1>
+  return (
+    <h1 className={header}>
+      Fullstack Boilerplate
+      <br />
+      <span> Web Application</span>
+    </h1>
+  )
 }
 
 export default Header
--- landing-page/src/components/hero/content/Subheader.js
+++ landing-page/src/components/hero/content/Subheader.js
@@ -2,9 +2,38 @@ import React from 'react'
 import { css } from 'linaria' // eslint-disable-line
 
 const subheader = css`
+  margin: 28px 0 32px var(--gutter);
+
+  /*******************/
+  /* Adapt text size */
+  /*******************/
+
   font-size: 1rem;
+  line-height: 21px;
   font-weight: 400;
-  margin: 28px 0 32px 12px;
+
+  @media screen and (max-width: 800px) and (min-width: 415px) {
+    font-size: 16px;
+    line-height: 19px;
+  }
+
+  @media screen and (max-width: 414px) and (min-width: 321px) {
+    font-size: 14px;
+    line-height: 16px;
+  }
+
+  @media screen and (max-width: 320px) {
+    font-size: 12px;
+    line-height: 14px;
+  }
+
+  /*********************/
+  /* Adapt text-align */
+  /*********************/
+
+  @media screen and (max-width: 540px) {
+    text-align: center;
+  }
 `
 
 function Subheader(props) {
