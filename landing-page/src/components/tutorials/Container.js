d24de69576690aa868acb1e3183528a722cca54b
guandjoy <guandjoy@gmail.com>
2019-06-27 07:10:32 +0300

    Rebuild tutorials

--- landing-page/src/components/tutorials/Container.js
+++ landing-page/src/components/tutorials/Container.js
@@ -3,31 +3,50 @@ import { css } from 'linaria' // eslint-disable-line
 
 const container = css`
   position: relative;
-  height: 100vh;
-  width: 100%;
-  background-color: #2e353b;
+  background-color: white;
 `
 
-const video = css`
-  position: relative;
-  padding: 0px auto 0px auto;
+const header = css`
+  margin: 0 auto 0 auto;
+  text-align: center;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 42px;
+
+  color: #444444;
+`
+
+const content = css`
+  margin: 0 auto 0 auto;
+`
+
+const element = css`
+  width: 408px;
+  height: 64px;
+  margin: 16px auto 16px auto;
+  background: #ffffff;
+  border: 1px solid #e3e3e3;
+  box-sizing: border-box;
+  border-radius: 8px;
 `
 
 function TutorialsContainer(props) {
   return (
     <div className={container}>
-      <h2>Tutorials</h2>
-      <div>
-        <iframe
-          className={video}
-          title="tutorials"
-          width="640"
-          height="360"
-          src="https://www.youtube.com/embed/videoseries?list=PLip1ThU0gOFb8GUDeuek69aalgyM4yQOI"
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
+      <h2 className={header}>Tutorials</h2>
+      <div className={content}>
+        <div className={element}>
+          01<span>Install</span>
+        </div>
+        <div className={element}>
+          02<span>Walkthrough</span>
+        </div>
+        <div className={element}>
+          03<span>Deploy landing on Netlify</span>
+        </div>
+        <div className={element}>
+          04<span>Deploy server on DigitalOcean</span>
+        </div>
       </div>
     </div>
   )
