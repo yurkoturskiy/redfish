1f09606ebfb8507c3914a9b746ba79f12847a86a
guandjoy <guandjoy@gmail.com>
2019-06-27 04:30:40 +0300

    Update logo

--- landing-page/src/components/header/Logo.js
+++ landing-page/src/components/header/Logo.js
@@ -1,28 +1,39 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import icon from '../../images/redfish-color-logo.svg'
+import RedfishLogoColor from '../../images/RedfishLogoColor'
 import { css } from 'linaria' // eslint-disable-line
 
 export const logo = css`
+  width: 126px;
+  height: 58px;
+
+  /* Adapt position */
+
   position: absolute;
   top: 32px;
-  left: 32px;
-  width: 44px;
-  height: 44px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  border-radius: 12px;
+  left: calc(18.18% + var(--gutter) / 2);
+
+  @media screen and (max-width: 1439px) and (min-width: 801px) {
+    left: calc(9.09% + var(--gutter) / 2);
+  }
+
+  @media screen and (max-width: 800px) and (min-width: 541px) {
+    left: calc(var(--gutter) * 2);
+  }
+
+  @media screen and (max-width: 540px) {
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+  }
 `
 
 function Logo(props) {
   return (
     <Link to="/">
-      <img
-        className={logo}
-        src={icon}
-        alt="redfish-logo"
-        height="44px"
-        width="44px"
-      />
+      <RedfishLogoColor className={logo} />
     </Link>
   )
 }
