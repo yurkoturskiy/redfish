3d5dd980771a1276a1915453f3eed4f2f03a566f
guandjoy <guandjoy@gmail.com>
2019-06-27 04:44:00 +0300

    Adapt flex direction

--- landing-page/src/components/hero/content/ActionBtnsWrapper.js
+++ landing-page/src/components/hero/content/ActionBtnsWrapper.js
@@ -3,8 +3,13 @@ import { css } from 'linaria' // eslint-disable-line
 
 const wrapper = css`
   display: flex;
+  flex-direction: row;
   align-items: center;
   margin: 0;
+
+  @media screen and (max-width: 540px) {
+    flex-direction: column;
+  }
 `
 
 function ActionBtnsWrapper(props) {
