93357c8493e78708acd291c10c35e14b4f432f6c
guandjoy <guandjoy@gmail.com>
2019-06-27 06:06:08 +0300

    Add favicon

--- landing-page/gatsby-config.js
+++ landing-page/gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: `Redfish`,
     siteUrl: `https://redfish-project.gq/`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@guandjoy`,
   },
   plugins: [
     'gatsby-plugin-linaria',
@@ -36,7 +36,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
