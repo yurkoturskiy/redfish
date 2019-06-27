780f12e8bb38e51ec93fddaf11b031e3a5a9f175
guandjoy <guandjoy@gmail.com>
2019-06-27 04:43:13 +0300

    Update video button

--- landing-page/src/components/hero/content/actionBtns/BrieflyAboutBtn.js
+++ landing-page/src/components/hero/content/actionBtns/BrieflyAboutBtn.js
@@ -1,20 +1,69 @@
 import React from 'react'
 import { css } from 'linaria' // eslint-disable-line
+import Button from '@material/react-button'
+// Local components
+import PlayIcon from '../../../../images/PlayIcon'
 
 const button = css`
+  position: relative;
   height: 88px;
-  width: 284px;
-  border: 1px solid lightgrey;
+  width: 245px;
   border-radius: 44px;
-  line-height: 88px;
-  text-align: center;
+  background: #ffffff;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
+  border: 0;
+  padding: 0;
+  --mdc-theme-primary: #444;
+
+  .mdc-button__icon {
+    position: absolute;
+    left: 32px;
+    height: 29px;
+    width: 29px;
+    fill: #595959;
+    padding-bottom: 2px;
+  }
+
+  .mdc-button__label {
+    font-weight: 300;
+    font-size: 1.125rem;
+    letter-spacing: 0;
+    line-height: 88px;
+    text-transform: initial;
+    margin-left: 16px;
+  }
+
+  /* Adapt size */
+
+  @media screen and (max-width: 800px) {
+    height: 72px;
+    width: 200px;
+    border-radius: 36px;
+
+    .mdc-button__icon {
+      height: 22px;
+      width: 22px;
+      left: 24px;
+    }
+
+    .mdc-button__label {
+      font-size: 1rem;
+      line-height: 72px;
+      margin-left: 8px;
+    }
+  }
 `
 
 function BrieflyAboutBtn(props) {
+  const leftIcon = <PlayIcon />
   return (
-    <div className={button} onClick={() => props.setDialog(true)}>
+    <Button
+      className={button}
+      icon={leftIcon}
+      onClick={() => props.setDialog(true)}
+    >
       Briefly about
-    </div>
+    </Button>
   )
 }
 
---
+++ landing-page/src/images/PlayIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PlayIcon(props) {
+  return (
+    <svg {...props} width="19px" height="29px" viewBox="0 0 19 29">
+      <path d="M17.8687 12.9547C18.8425 13.7548 18.8425 15.2452 17.8687 16.0453L3.51916 27.8351C2.21401 28.9074 0.24951 27.9789 0.24951 26.2898L0.249511 2.71023C0.249511 1.02106 2.21401 0.0925926 3.51916 1.16492L17.8687 12.9547Z" />
+    </svg>
+  )
+}
+
+export default PlayIcon
