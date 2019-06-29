7743350eba1a13bd9b05377bca90375ea09cd991
guandjoy <guandjoy@gmail.com>
2019-06-29 20:18:05 +0300

    Add hero image

---
+++ landing-page/src/images/HeroImage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function HeroImage(props) {
+  return (
+    <svg width="459" height="608" viewBox="0 0 459 608" fill="none" {...props}>
+      <path
+        d="M276.63 2.0661e-05L419.682 533.877L54.2611 276.628L276.63 2.0661e-05Z"
+        fill="#FF4A64"
+      />
+      <path
+        d="M458.208 94.1825L416.475 83L294 540.081L335.734 551.264L458.208 94.1825Z"
+        fill="#1D79FF"
+      />
+      <path
+        d="M218.78 468.318L46.5238 486.797L149.643 250.695L218.78 468.318Z"
+        fill="#1DB355"
+      />
+    </svg>
+  )
+}
+
+export default HeroImage
