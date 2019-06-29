628217b4ef9b419ee5ae0f68e42c2adbcdbb074d
guandjoy <guandjoy@gmail.com>
2019-06-29 20:15:49 +0300

    Add TutorialsBtn component

---
+++ landing-page/src/components/hero/content/actionBtns/TutorialsBtn.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from '../../../styledUIElements/Button'
+
+function TutorialsBtn(props) {
+  const scrollToTutorials = e => {
+    let tutorialsHeader = document.getElementById('tutorials')
+
+    console.log(window.pageYOffset)
+    let scroll =
+      tutorialsHeader.getBoundingClientRect().top +
+      document.documentElement.scrollTop
+    console.log(
+      'offset top',
+      tutorialsHeader.getBoundingClientRect().top +
+        document.documentElement.scrollTop
+    )
+    window.scroll({
+      top: scroll,
+      left: 0,
+      behavior: 'smooth',
+    })
+  }
+  return <Button onClick={e => scrollToTutorials(e)}>Tutorials</Button>
+}
+
+export default TutorialsBtn
