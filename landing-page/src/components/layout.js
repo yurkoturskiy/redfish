98e1a7fc868a93dc00373d296b226acb51b285a0
guandjoy <guandjoy@gmail.com>
2019-06-29 19:30:25 +0300

    Add variables

--- landing-page/src/components/layout.js
+++ landing-page/src/components/layout.js
@@ -16,15 +16,28 @@ export const globals = css`
     @import url('https://fonts.googleapis.com/css?family=Fira+Mono&display=swap');
 
     :root {
+      /********/
+      /* Grid */
+      /********/
+
       --gutter: 24px;
+      --half-gutter: calc(var(--gutter) / 2);
+      --grid-margin: 12px;
       --columns: 12;
 
-      --red-two: #ffedeb;
-      --green-two: #d0eedb;
-      --green-seven: #304538;
-      --green-eight: #344239;
-      --green-nine: #2b352f;
-      --green-ten: #151615;
+      --grid-unit: calc(100% / var(--columns));
+      --col-one: calc(var(--grid-unit) + var(--half-gutter));
+      --col-two: calc(var(--grid-unit) * 2 + var(--half-gutter));
+      --col-three: calc(var(--grid-unit) * 3 + var(--half-gutter));
+      --col-four: calc(var(--grid-unit) * 4 + var(--half-gutter));
+      --col-five: calc(var(--grid-unit) * 5 + var(--half-gutter));
+      --col-six: calc(var(--grid-unit) * 6 + var(--half-gutter));
+      --col-seven: calc(var(--grid-unit) * 7 + var(--half-gutter));
+      --col-eight: calc(var(--grid-unit) * 8 + var(--half-gutter));
+      --col-nine: calc(var(--grid-unit) * 9 + var(--half-gutter));
+      --col-ten: calc(var(--grid-unit) * 10 + var(--half-gutter));
+      --col-eleven: calc(var(--grid-unit) * 11 + var(--half-gutter));
+      --col-twelve: calc(var(--grid-unit) * 12 + var(--half-gutter));
 
       @media screen and (max-width: 800px) and (min-width: 415px) {
         --columns: 8;
@@ -34,6 +47,21 @@ export const globals = css`
         --columns: 4;
         --gutter: 16px;
       }
+
+      /**********/
+      /* Colors */
+      /**********/
+
+      --red: #ff4a64;
+      --green: #1db355;
+      --blue: #1d79ff;
+
+      --red-two: #ffedeb;
+      --green-two: #d0eedb;
+      --green-seven: #304538;
+      --green-eight: #344239;
+      --green-nine: #2b352f;
+      --green-ten: #151615;
     }
 
     body {
