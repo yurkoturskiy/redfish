ab1cd6bef99a165564cfa40c2b4af74f5e55de8b
guandjoy <guandjoy@gmail.com>
2019-06-29 20:23:54 +0300

    Colorize subheader

--- landing-page/src/components/hero/content/Subheader.js
+++ landing-page/src/components/hero/content/Subheader.js
@@ -3,29 +3,39 @@ import { css } from 'linaria' // eslint-disable-line
 
 const subheader = css`
   margin: 28px 0 32px var(--gutter);
-  color: var(--green-eight);
+
+  #blue {
+    color: var(--blue);
+  }
+  #red {
+    color: var(--red);
+  }
+
+  #green {
+    color: var(--green);
+  }
 
   /*******************/
   /* Adapt text size */
   /*******************/
 
-  font-size: 1rem;
-  line-height: 21px;
-  font-weight: 400;
+  font-weight: bold;
+  font-size: 1.125rem
+  line-height: 1.3125rem;
 
   @media screen and (max-width: 800px) and (min-width: 415px) {
-    font-size: 16px;
-    line-height: 19px;
+    font-size: 1rem;
+    line-height: 1.1875rem;
   }
 
   @media screen and (max-width: 414px) and (min-width: 321px) {
     font-size: 14px;
-    line-height: 16px;
+    line-height: 0.875rem;
   }
 
   @media screen and (max-width: 320px) {
-    font-size: 12px;
-    line-height: 14px;
+    font-size: 0.75rem;
+    line-height: 0.875rem;
   }
 
   /*********************/
@@ -38,7 +48,12 @@ const subheader = css`
 `
 
 function Subheader(props) {
-  return <h2 className={subheader}>ReactJS, GraphQL, and Django inside</h2>
+  return (
+    <h2 className={subheader}>
+      <span id="blue">ReactJS</span> <span id="red">GraphQL</span>{' '}
+      <span id="green">Django</span>
+    </h2>
+  )
 }
 
 export default Subheader
