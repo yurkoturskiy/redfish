6257770238dcdf9fa6e4a8ea5359ced41036a7b4
guandjoy <guandjoy@gmail.com>
2019-06-29 20:26:34 +0300

    Update hero's components changes

--- landing-page/src/components/HeroContainer.js
+++ landing-page/src/components/HeroContainer.js
@@ -6,31 +6,45 @@ import ContentWrapper from './hero/ContentWrapper'
 import VideoDialog from './hero/content/VideoDialog'
 import Header from './hero/content/Header'
 import Subheader from './hero/content/Subheader'
+import Image from './hero/content/Image'
 //  ** Action buttons
 import ActionBtnsWrapper from './hero/content/ActionBtnsWrapper'
 import BrieflyAboutBtn from './hero/content/actionBtns/BrieflyAboutBtn'
 import GitHubPageBtn from './hero/content/actionBtns/GitHubPageBtn'
 import GoToAppBtn from './hero/content/actionBtns/GoToAppBtn'
+import TutorialsBtn from './hero/content/actionBtns/TutorialsBtn'
+//  ** Others
 import GitClone from './hero/content/GitClone'
 
 const container = css`
   position: relative;
   height: 86vh;
   background-color: var(--red-two);
+  padding-left: var(--grid-margin);
+  padding-right: var(--grid-margin);
+`
+
+const buttons = css`
+  margin-left: 28px;
 `
 
 function HeroContainer(props) {
   const [dialog, setDialog] = useState(false)
   return (
     <div className={container}>
+      <Image />
       <ContentWrapper>
         <Header />
         <Subheader />
+
         {dialog && <VideoDialog setDialog={setDialog} />}
         <ActionBtnsWrapper>
           <BrieflyAboutBtn setDialog={setDialog} />
-          <GoToAppBtn />
-          <GitHubPageBtn />
+          <div className={buttons}>
+            <GoToAppBtn />
+            <TutorialsBtn />
+            <GitHubPageBtn />
+          </div>
         </ActionBtnsWrapper>
       </ContentWrapper>
       <GitClone />
