f1fe85f5f1c32889c48165b0ee62075e2cf4ffce
guandjoy <guandjoy@gmail.com>
2019-07-22 21:09:41 +0300

    Add support of multiple environments

--- src/landing-page/gatsby-config.js
+++ src/landing-page/gatsby-config.js
@@ -1,5 +1,8 @@
+let activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
+
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
+  path: `.env.${activeEnv}`,
 })
 
 const dynamicPlugins = []
