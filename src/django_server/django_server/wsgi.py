5d07b2d8e005387d6436476eb9930c4ab6c03932
guandjoy <guandjoy@gmail.com>
2019-07-24 17:35:53 +0300

    Able to set environment as GitHub or GitLab deployment branch

--- src/django_server/django_server/wsgi.py
+++ src/django_server/django_server/wsgi.py
@@ -1,7 +1,8 @@
 """
 WSGI config for django_server project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It support local and serverless environments.
+Deploy app with `now` via GitHub, GitLab, or locally
 
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
@@ -10,17 +11,57 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-try:
-	environment = os.environ['DJANGO_ENV']
-except:
-	environment = 'local'
 
-# Import env variables
-exec(f'from django_server.env import {environment} as env_variables')
-print(f'set {environment} environment')
-# Set settings depends on environment
+try:
+	'''
+	Retreive ENV_AS_BRANCH environment variable, provided by now.json file
+	Set environment as name of the branch that the app was deployed using
+	'''
+	environment_as_branch = eval(os.environ['ENV_AS_BRANCH'])
+except:
+	environment_as_branch = False
+
+
+if environment_as_branch:
+	# GitHub
+	try:
+		# Set environment as the branch that the app was deployed using
+		github_branch = os.environ['NOW_GITHUB_COMMIT_REF']
+		environment = github_branch
+	except:
+		environment = False
+
+
+	# GitLab
+	try:
+		# Set environment as the branch that the app was deployed using
+		gitlab_branch = os.environ['GITLAB_COMMIT_REF']
+		environment = gitlab_branch
+	except:
+		environment = False
+
+
+# Local
+if not environment:
+	try:
+		# If local `now` deployment
+		environment = os.environ['DJANGO_ENV']
+	except:
+		# If local `manage.py` runserver
+		environment = 'local'
+
+
+# Set settings as environment name
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'django_server.settings.{environment}')
-# Set env variables
+
+
+# Update environment variables from env.py based on environment name
+exec(f'from django_server.env import {environment} as env_variables')
 os.environ.update(env_variables)
+
+
+# Logging
+print(f'set {environment} environment')
+
 
 application = get_wsgi_application()
