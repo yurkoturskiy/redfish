125a0a149a588a073aeb4892e719c8e0bc42606e
guandjoy <guandjoy@gmail.com>
2019-07-24 13:39:58 +0300

    Refactor environments

--- src/django_server/django_server/env.py
+++ src/django_server/django_server/env.py
@@ -1,18 +1,25 @@
-# Development environment variables
-development = {
+# Local environment variables
+local = {
 	'SERVER_URL': 'http://localhost:9000',
 	'LANDING_URL': 'http://localhost:8000',
 	'APPLICATION_URL': 'http://localhost:3006'
 }
 
-# Production environment variables
-production = {
+# Master branch env variables
+master = {
 	'SERVER_URL': 'https://redfish-server-prod.guandjoy.now.sh',
 	'LANDING_URL': 'https://redfish-project.gq',
 	'APPLICATION_URL': 'https://app.redfish-project.gq'
 }
 
-# Staging environment variables
+# Development environment variables
+development = {
+	'SERVER_URL': 'https://redfish-server-development.guandjoy.now.sh',
+	'LANDING_URL': 'http://localhost:8000',
+	'APPLICATION_URL': 'http://localhost:3006'
+}
+
+# Staging branch env variables
 staging = {
 	'SERVER_URL': 'https://redfish-server-staging.guandjoy.now.sh',
 	'LANDING_URL': 'https://staging.redfish-project.gq',
