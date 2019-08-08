40b6c3cb6d6a099519ce26769631adf1df7433e7
guandjoy <guandjoy@gmail.com>
2019-08-09 02:33:57 +0300

    Add <br/> tag after 'boilerplate'

--- src/landing-page/src/components/hero/content/Header.js
+++ src/landing-page/src/components/hero/content/Header.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function Header() {
-  return <h1 className="header">Boilerplate&nbsp;Web App</h1>
+  return (
+    <h1 className="header">
+      Boilerplate
+      <br />
+      Web App
+    </h1>
+  )
 }
 
 export default Header
