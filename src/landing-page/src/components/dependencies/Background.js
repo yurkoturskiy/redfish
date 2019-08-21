f4e032f62b24d9e92860ff5eec00e0e86d43e2f5
guandjoy <guandjoy@gmail.com>
2019-08-21 23:25:00 +0300

    Improve component

--- src/landing-page/src/components/dependencies/Background.js
+++ src/landing-page/src/components/dependencies/Background.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { morphing, path } from 'primitivo-svg'
 
-var morphingParams = {
-  numOfKeyPaths: 5,
-  loop: true,
-}
+// var morphingParams = {
+//   numOfKeyPaths: 5,
+//   loop: true,
+// }
 
 var pathsParams = {
   numOfSegments: 4,
@@ -40,33 +40,78 @@ var pathsParams = {
   ],
 }
 
-const one = morphing(morphingParams, pathsParams)
-const two = morphing(morphingParams, pathsParams)
+const paths = () => {
+  var morphingParams = {
+    numOfKeyPaths: 3,
+    loop: true,
+  }
+  var set = []
+  for (let i = 0; i < 10; i++) {
+    let x = -100 + i * 45
+    let y = -200 + i * 40
+    let width = 1920 - i * (1920 / 10)
+    let height = 1920 - i * (1920 / 10)
+    let centerX = width / 2
+    let centerY = height / 2
+    let pathsParams = {
+      numOfSegments: 4,
+      depth: 0,
+      x,
+      y,
+      width,
+      height,
+      centerX,
+      centerY,
+      rotate: 0,
+      numOfGroups: 3,
+      incircle: true,
+      groups: [
+        {
+          type: 'radial',
+          distance: [0.8, 1],
+          round: 1,
+          lengthBasedRound: true,
+        },
+        {
+          type: 'radial',
+          distance: [0.8, 1],
+          round: 1,
+          lengthBasedRound: false,
+        },
+        {
+          type: 'radial',
+          distance: [0.8, 1],
+          round: 1,
+          lengthBasedRound: false,
+        },
+      ],
+    }
+    set[i] = morphing(morphingParams, pathsParams).dValues
+  }
+  return set
+}
 
-const pathObj = path()
-console.log('background path', pathObj)
+var a = paths()
+var pathsSVG = a.map((path, index) => {
+  return (
+    <path
+      stroke={index === a.length - 1 ? '#1D79FF' : '#1D79FF'}
+      strokeWidth="1"
+      fill="#1D79FF"
+      fillOpacity={index === a.length - 1 ? '1' : '0'}
+    >
+      <animate
+        attributeName="d"
+        repeatCount="indefinite"
+        dur="120s"
+        values={path}
+      />
+    </path>
+  )
+})
 
 function Background() {
-  return (
-    <svg className="background">
-      <path stroke="black" strokeWidth="1" fill="white" fill-opacity="0">
-        <animate
-          attributeName="d"
-          repeatCount="indefinite"
-          dur="10s"
-          values={one.dValues}
-        />
-      </path>
-      <path stroke="black" strokeWidth="1" fill="white" fill-opacity="0">
-        <animate
-          attributeName="d"
-          repeatCount="indefinite"
-          dur="10s"
-          values={two.dValues}
-        />
-      </path>
-    </svg>
-  )
+  return <svg className="background">{pathsSVG}</svg>
 }
 
 export default Background
