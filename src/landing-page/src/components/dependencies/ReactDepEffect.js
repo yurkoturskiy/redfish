05618c16eb12d8a8be4a159047df2af122b63e37
guandjoy <guandjoy@gmail.com>
2019-09-15 00:08:48 +0300

    Update ReactDepEffect.js

--- src/landing-page/src/components/dependencies/ReactDepEffect.js
+++ src/landing-page/src/components/dependencies/ReactDepEffect.js
@@ -1,19 +1,15 @@
 import React from 'react'
-import { morphing, path } from 'primitivo-svg'
-
-var morphingParams = {
-  numOfKeyPaths: 2,
-  loop: true,
-}
+import { path } from 'primitivo-svg'
 
 let x = 0
 let y = 0
-let width = 640
-let height = 480
+let width = 128
+let height = 128
 let centerX = width / 2
 let centerY = height / 2
-let pathsParams = {
-  numOfSegments: 3,
+
+var shapePathParams = {
+  numOfSegments: 4,
   depth: 0,
   x,
   y,
@@ -21,74 +17,24 @@ let pathsParams = {
   height,
   centerX,
   centerY,
-  rotate: 0,
-  numOfGroups: 3,
+  rotate: 25,
+  numOfGroups: 1,
   groups: [
     {
       type: 'radial',
       incircle: true,
       distance: [0.8, 1],
-      round: [0.6, 1],
+      round: 0,
       lengthBasedRound: true,
     },
-    {
-      type: 'radial',
-      incircle: true,
-      distance: [0.8, 1],
-      round: [0.6, 1],
-      lengthBasedRound: false,
-    },
-    {
-      type: 'radial',
-      incircle: true,
-      distance: [0.8, 1],
-      round: [0.6, 1],
-      lengthBasedRound: false,
-    },
   ],
 }
-
-var glarePathParams = {
-  numOfSegments: 3,
-  depth: 0,
-  x: 100,
-  y: 120,
-  width: 50,
-  height: 50,
-  centerX: 25,
-  centerY: 25,
-  rotate: 180,
-  numOfGroups: 2,
-  groups: [
-    {
-      type: 'radial',
-      incircle: true,
-      distance: [0.8, 1],
-      round: [0.6, 1],
-    },
-    {
-      type: 'radial',
-      incircle: true,
-      distance: [0.8, 1],
-      round: [0.7, 1],
-    },
-  ],
-}
-const blob = path(glarePathParams)
-const { dValues } = morphing(morphingParams, pathsParams)
+const blob = path(shapePathParams)
 
 function ReactDepEffect(props) {
   return (
     <svg width="660" height="500" id="react-dep-ring">
-      <path stroke="#1D79FF" strokeWidth="10" fillOpacity="0">
-        <animate
-          attributeName="d"
-          repeatCount="indefinite"
-          dur="60s"
-          values={dValues}
-        />
-      </path>
-      <path d={blob.d} fill="red" />
+      <path d={blob.d} stroke="#1D79FF" strokeWidth="10" fillOpacity="0" />
     </svg>
   )
 }
