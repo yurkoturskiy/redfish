6bb8139bcf3f3e96354e8b9f9d1460dae7d11dfc
guandjoy <guandjoy@gmail.com>
2019-09-28 00:00:38 +0300

    Add links

--- src/landing-page/src/components/dependencies/Container.js
+++ src/landing-page/src/components/dependencies/Container.js
@@ -21,42 +21,54 @@ function DependenciesContainer() {
           <ReactDepEffect />
         </div>
         <div className="cardsWrapper" id="react-cards">
-          <div className="card">
-            <GatsbyIcon className="icon" />
-            <h4 className="purpose">Landing page</h4>
-            <h2 className="name">GatsbyJS</h2>
-          </div>
-          <div className="card">
-            <ApolloIcon className="icon" />
-            <h4 className="purpose">Queries</h4>
-            <h2 className="name">Apollo GraphQL Client</h2>
-          </div>
-          <div className="card">
-            <CreateReactAppIcon className="icon" />
-            <h4 className="purpose">Application</h4>
-            <h2 className="name">Create React App</h2>
-          </div>
-          <div className="card">
-            <LinariaIcon className="icon" />
-            <h4 className="purpose">SVG effects</h4>
-            <h2 className="name">Primitivo-SVG</h2>
-          </div>
+          <a href="https://www.gatsbyjs.org/">
+            <div className="card">
+              <GatsbyIcon className="icon" />
+              <h4 className="purpose">Landing page</h4>
+              <h2 className="name">GatsbyJS</h2>
+            </div>
+          </a>
+          <a href="https://www.apollographql.com">
+            <div className="card">
+              <ApolloIcon className="icon" />
+              <h4 className="purpose">Queries</h4>
+              <h2 className="name">Apollo GraphQL Client</h2>
+            </div>
+          </a>
+          <a href="https://create-react-app.dev/">
+            <div className="card">
+              <CreateReactAppIcon className="icon" />
+              <h4 className="purpose">Application</h4>
+              <h2 className="name">Create React App</h2>
+            </div>
+          </a>
+          <a href="https://github.com/guandjoy/primitivo-svg">
+            <div className="card">
+              <LinariaIcon className="icon" />
+              <h4 className="purpose">SVG effects</h4>
+              <h2 className="name">Primitivo-SVG</h2>
+            </div>
+          </a>
         </div>
         <div className="section" id="django-section">
           <h3>Django Dependencies</h3>
           <DjangoDepEffect />
         </div>
         <div className="cardsWrapper" id="django-cards">
-          <div className="card">
-            <GrapheneIcon className="icon" />
-            <h4 className="purpose">GraphQL Server</h4>
-            <h2 className="name">Graphene-Python</h2>
-          </div>
-          <div className="card">
-            <GatsbyIcon className="icon" />
-            <h4 className="purpose">Authentication</h4>
-            <h2 className="name">django-rest-auth</h2>
-          </div>
+          <a href="https://graphene-python.org/">
+            <div className="card">
+              <GrapheneIcon className="icon" />
+              <h4 className="purpose">GraphQL Server</h4>
+              <h2 className="name">Graphene-Python</h2>
+            </div>
+          </a>
+          <a href="https://django-rest-auth.readthedocs.io/en/latest/#">
+            <div className="card">
+              <GatsbyIcon className="icon" />
+              <h4 className="purpose">Authentication</h4>
+              <h2 className="name">django-rest-auth</h2>
+            </div>
+          </a>
         </div>
       </div>
     </div>
