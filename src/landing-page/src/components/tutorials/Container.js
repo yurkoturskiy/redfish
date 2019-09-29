4c020988f92c2da4c056ede789f18ed22a6c0e6d
guandjoy <guandjoy@gmail.com>
2019-09-29 04:20:27 +0300

    Implement flex layout

--- src/landing-page/src/components/tutorials/Container.js
+++ src/landing-page/src/components/tutorials/Container.js
@@ -1,33 +1,32 @@
 import React from 'react'
-import Button from '../styledUIElements/Button'
+import TransitionEffectButton from '../styledUIElements/TransitionEffectButton'
+import TutorialsBtn from '../hero/content/actionBtns/TutorialsBtn'
 
 function TutorialsContainer() {
   return (
     <div className="tutorials-container">
-      <h2 id="tutorials-header">Tutorials</h2>
-      <div className="content">
-        <div className="element">
-          <span>
-            <strong>01</strong>Install
-          </span>
-        </div>
-        <div className="element">
-          <span>
-            <strong>02</strong>Walkthrough
-          </span>
-        </div>
-        <div className="element">
-          <span>
-            <strong>03</strong>Deploy landing on Netlify
-          </span>
-        </div>
-        <div className="element">
-          <span>
-            <strong>04</strong>Deploy server on DigitalOcean
-          </span>
-        </div>
+      <h2>Tutorials</h2>
+      <div className="element">
+        <span>
+          <strong>01</strong>Install
+        </span>
       </div>
-      <Button>View on YouTube</Button>
+      <div className="element">
+        <span>
+          <strong>02</strong>Walkthrough
+        </span>
+      </div>
+      <div className="element">
+        <span>
+          <strong>03</strong>Deploy landing on Netlify
+        </span>
+      </div>
+      <div className="element">
+        <span>
+          <strong>04</strong>Deploy server on DigitalOcean
+        </span>
+      </div>
+      <TransitionEffectButton to="/docs/">View More</TransitionEffectButton>
     </div>
   )
 }
