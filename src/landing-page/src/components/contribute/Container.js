30ea5f0e6d149ccf196c0fc7987cc566682e819d
guandjoy <guandjoy@gmail.com>
2019-09-29 04:26:12 +0300

    Update button

--- src/landing-page/src/components/contribute/Container.js
+++ src/landing-page/src/components/contribute/Container.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from '../styledUIElements/Button'
+import ExternalLinkButton from '../styledUIElements/ExternalLinkButton'
 
 function ContributeContainer() {
   return (
@@ -9,7 +9,9 @@ function ContributeContainer() {
         Redfish is belong to open source community. We have what to improve, so
         feel free to participate.
       </p>
-      <Button id="send-email">Send email</Button>
+      <ExternalLinkButton to="https://github.com/guandjoy/Redfish">
+        Send email
+      </ExternalLinkButton>
     </div>
   )
 }
