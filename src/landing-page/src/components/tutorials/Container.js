5764490bf00a005932f4faf5ec93c892646dee69
guandjoy <guandjoy@gmail.com>
2019-09-29 08:25:51 +0300

    Update tutorials section

--- src/landing-page/src/components/tutorials/Container.js
+++ src/landing-page/src/components/tutorials/Container.js
@@ -1,31 +1,37 @@
 import React from 'react'
 import TransitionEffectButton from '../styledUIElements/TransitionEffectButton'
+import Background from './Background'
 
 function TutorialsContainer() {
   return (
     <div className="tutorials-container">
-      <h2>Tutorials</h2>
-      <div className="element">
-        <span>
-          <strong>01</strong>Install
-        </span>
+      <Background />
+      <div className="tutorials-content">
+        <h2>Tutorials</h2>
+        <div className="element">
+          <span>
+            <strong>01</strong>Install
+          </span>
+        </div>
+        <div className="element">
+          <span>
+            <strong>02</strong>Walkthrough
+          </span>
+        </div>
+        <div className="element">
+          <span>
+            <strong>03</strong>Deploy landing on Netlify
+          </span>
+        </div>
+        <div className="element">
+          <span>
+            <strong>04</strong>Deploy server on DigitalOcean
+          </span>
+        </div>
+        <TransitionEffectButton id="tutorials-view-more-button" to="/docs/">
+          View More
+        </TransitionEffectButton>
       </div>
-      <div className="element">
-        <span>
-          <strong>02</strong>Walkthrough
-        </span>
-      </div>
-      <div className="element">
-        <span>
-          <strong>03</strong>Deploy landing on Netlify
-        </span>
-      </div>
-      <div className="element">
-        <span>
-          <strong>04</strong>Deploy server on DigitalOcean
-        </span>
-      </div>
-      <TransitionEffectButton to="/docs/">View More</TransitionEffectButton>
     </div>
   )
 }
