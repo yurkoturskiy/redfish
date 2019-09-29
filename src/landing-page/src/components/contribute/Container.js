373924bbdcd28ea5b54ae93a54c05418065e5aed
guandjoy <guandjoy@gmail.com>
2019-09-29 07:08:47 +0300

    Remove send email button

--- src/landing-page/src/components/contribute/Container.js
+++ src/landing-page/src/components/contribute/Container.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ExternalLinkButton from '../styledUIElements/ExternalLinkButton'
 
 function ContributeContainer() {
   return (
@@ -9,9 +8,6 @@ function ContributeContainer() {
         Redfish is belong to open source community. We have what to improve, so
         feel free to participate.
       </p>
-      <ExternalLinkButton to="https://github.com/guandjoy/Redfish">
-        Send email
-      </ExternalLinkButton>
     </div>
   )
 }
