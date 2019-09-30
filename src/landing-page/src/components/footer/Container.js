a69b00766433f9b6f57c724060b2b4148aa73923
guandjoy <guandjoy@gmail.com>
2019-10-01 01:38:08 +0300

    Move triangles pattern to footer container

--- src/landing-page/src/components/footer/Container.js
+++ src/landing-page/src/components/footer/Container.js
@@ -1,10 +1,12 @@
 import React from 'react'
 // Local components
 import FooterPattern from '../../images/FooterPattern'
+import Background from './Background'
 
 function FooterContainer(props) {
   return (
     <div className="footer-wrapper">
+      <Background />
       <FooterPattern className="footer-pattern" />
     </div>
   )
