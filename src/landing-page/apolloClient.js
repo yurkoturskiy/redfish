1a9731174a86655644a17cf8150497b1b0574315
guandjoy <guandjoy@gmail.com>
2019-10-21 01:16:17 +0300

    Update and cleanup packages

--- src/landing-page/apolloClient.js
+++ src/landing-page/apolloClient.js
@@ -4,7 +4,6 @@ import { ApolloLink } from 'apollo-link'
 import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { withClientState } from 'apollo-link-state'
-import { RestLink } from 'apollo-link-rest'
 
 const cache = new InMemoryCache()
 
