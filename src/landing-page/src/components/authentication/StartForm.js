d839d21b1136eb10469ba3b060a508b3d91daa39
guandjoy <guandjoy@gmail.com>
2019-10-21 00:07:44 +0300

    Update start form

--- src/landing-page/src/components/authentication/StartForm.js
+++ src/landing-page/src/components/authentication/StartForm.js
@@ -6,14 +6,11 @@ import AuthWithGitHub from './AuthWithGitHub'
 
 function StartForm(props) {
   return (
-    <div className="form-card">
-      <h5 className="header">Authorize to continue, please</h5>
+    <div className="authentication-start-form">
+      <h5 className="header">Pick the way to go</h5>
       <AuthWithFacebook>Continue with Facebook</AuthWithFacebook>
       <AuthWithGitHub>Continue with GitHub</AuthWithGitHub>
-      <div className="separator">
-        <p>r use email</p>
-      </div>
-      <div className="buttonsWrapper">
+      <div className="start-form-buttons-wrapper">
         <Button
           className="material-button"
           outlined={true}
