ba2373db9ad0618e14d6453b830339d66bc941bb
guandjoy <guandjoy@gmail.com>
2019-11-03 01:10:47 +0200

    Refactor SECRET_KEY declaration

--- src/django_server/django_server/settings_example/master.py
+++ src/django_server/django_server/settings_example/master.py
@@ -5,9 +5,6 @@ SERVER_URL = 'https://redfish-server.guandjoy.now.sh'
 LANDING_URL = 'https://redfish-project.gq'
 APPLICATION_URL = 'https://app.redfish-project.gq'
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
--- src/django_server/django_server/settings_example/staging.py
+++ src/django_server/django_server/settings_example/staging.py
@@ -5,9 +5,6 @@ SERVER_URL = 'https://redfish-server-staging.guandjoy.now.sh'
 LANDING_URL = 'https://staging.redfish-project.gq'
 APPLICATION_URL = 'https://app.staging.redfish-project.gq' 
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
