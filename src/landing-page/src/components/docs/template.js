f610007cefc13934f6bf475d12cad6d4819fa8f3
guandjoy <guandjoy@gmail.com>
2019-11-07 23:27:25 +0200

    Add or update keywords

--- src/landing-page/src/components/docs/template.js
+++ src/landing-page/src/components/docs/template.js
@@ -6,10 +6,23 @@ import SandwichButton from './SandwichButton'
 import Menu from './Menu'
 import Layout from '../layout'
 import TransitionLink from 'gatsby-plugin-transition-link'
+import SEO from '../seo'
 
 export default function PageTemplate(props) {
   return (
     <Layout path={props.path}>
+      <SEO
+        title="Docs"
+        keywords={[
+          `redfish`,
+          `application`,
+          `react`,
+          `gatsby`,
+          `django`,
+          `python`,
+          `graphql`,
+        ]}
+      />
       <div className="docs">
         <Menu />
         <div className="content">
