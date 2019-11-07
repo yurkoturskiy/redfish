3d85631a7a16f77b5a72d478c3487bfb4930feb7
guandjoy <guandjoy@gmail.com>
2019-11-08 01:07:29 +0200

    Refactor class component to functional

--- src/landing-page/src/components/authentication/forms/LoginForm.js
+++ src/landing-page/src/components/authentication/forms/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Field } from 'formik'
 import { Link } from 'gatsby'
 import { navigate } from 'gatsby'
@@ -7,73 +7,68 @@ import FormikMaterialTextField from '../../styledUIElements/FormikMaterialTextFi
 import Button from '@material/react-button'
 import Checkbox from '@material/react-checkbox'
 
-class LoginForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      passwordVisibilityCondition: false,
-    }
-    this.switchPasswordVisibility = this.switchPasswordVisibility.bind(this)
-  }
-  switchPasswordVisibility() {
-    this.setState({
-      passwordVisibilityCondition: !this.state.passwordVisibilityCondition,
-    })
-  }
-  render() {
-    const { status, isSubmitting } = this.props
-    return (
-      <Form>
-        <h3 className="form-header">Login to continue</h3>
-        <Field
-          id="username"
-          label="username or email"
-          name="username"
-          type="text"
-          component={FormikMaterialTextField}
-        />
-        <Field
-          id="password"
-          label="password"
-          name="password"
-          type={this.state.passwordVisibilityCondition ? 'text' : 'password'}
-          tralingIcon={
-            this.state.passwordVisibilityCondition
-              ? 'visibility'
-              : 'visibility_off'
-          }
-          tralingIconOnClick={this.switchPasswordVisibility}
-          component={FormikMaterialTextField}
-        />
-        <div className="subform-container">
-          {status && status.non_field_errors && (
-            <div className="non-fields-error">
-              <span>{status.non_field_errors}</span>
-            </div>
-          )}
-          <div className="form-buttons-wrapper">
-            <Button
-              type="button"
-              className="material-button"
-              disabled={isSubmitting}
-              onClick={() => this.props.setRoute('password-reset')}
-            >
-              Reset password
-            </Button>
-            <Button
-              unelevated
-              type="submit"
-              className="material-button"
-              solid={'true'}
-              disabled={isSubmitting}
-            >
-              Submit
-            </Button>
-          </div>
-        </div>
-      </Form>
+function LoginForm(props) {
+  const { status, isSubmitting } = props
+  const [
+    passwordVisibilityCondition,
+    setPasswordVisibilityCondition,
+  ] = useState(false)
+
+  const switchPasswordVisibility = () => {
+    setPasswordVisibilityCondition(
+      passwordVisibilityCondition => !passwordVisibilityCondition
     )
   }
+
+  return (
+    <Form>
+      <h3 className="form-header">Login to continue</h3>
+      <Field
+        id="username"
+        label="username or email"
+        name="username"
+        type="text"
+        component={FormikMaterialTextField}
+      />
+      <Field
+        id="password"
+        label="password"
+        name="password"
+        type={passwordVisibilityCondition ? 'text' : 'password'}
+        tralingIcon={
+          passwordVisibilityCondition ? 'visibility' : 'visibility_off'
+        }
+        tralingIconOnClick={switchPasswordVisibility}
+        component={FormikMaterialTextField}
+      />
+      <div className="subform-container">
+        {status && status.non_field_errors && (
+          <div className="non-fields-error">
+            <span>{status.non_field_errors}</span>
+          </div>
+        )}
+        <div className="form-buttons-wrapper">
+          <Button
+            type="button"
+            className="material-button"
+            disabled={isSubmitting}
+            onClick={() => props.setRoute('password-reset')}
+          >
+            Reset password
+          </Button>
+          <Button
+            unelevated
+            type="submit"
+            className="material-button"
+            solid={'true'}
+            disabled={isSubmitting}
+          >
+            Submit
+          </Button>
+        </div>
+      </div>
+    </Form>
+  )
 }
 
 export default LoginForm
--- src/landing-page/src/components/authentication/forms/RegistrationForm.js
+++ src/landing-page/src/components/authentication/forms/RegistrationForm.js
@@ -1,86 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Field } from 'formik'
 // Local components
 import FormikMaterialTextField from '../../styledUIElements/FormikMaterialTextField'
 import Button from '@material/react-button'
 
-class RegistrationForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      passwordVisibilityCondition: false,
-    }
-    this.switchPasswordVisibility = this.switchPasswordVisibility.bind(this)
-  }
-  switchPasswordVisibility() {
-    this.setState({
-      passwordVisibilityCondition: !this.state.passwordVisibilityCondition,
-    })
-  }
-  render() {
-    const {
-      status,
-      isSubmitting,
-      passwordOnChange,
-      onChange,
-      passwordHelperText,
-    } = this.props
-    return (
-      <Form>
-        <h3 className="form-header">Sign Up</h3>
-        <Field
-          id="username"
-          label="Username"
-          name="username"
-          type="text"
-          component={FormikMaterialTextField}
-        />
-        <Field
-          id="email"
-          label="email"
-          name="email"
-          type="email"
-          component={FormikMaterialTextField}
-        />
-        <Field
-          id="password"
-          label="password"
-          name="password1"
-          type={this.state.passwordVisibilityCondition ? 'text' : 'password'}
-          tralingIcon={
-            this.state.passwordVisibilityCondition
-              ? 'visibility'
-              : 'visibility_off'
-          }
-          tralingIconOnClick={this.switchPasswordVisibility}
-          component={FormikMaterialTextField}
-          onChange={e => {
-            onChange(e)
-            passwordOnChange(e)
-          }}
-          helperText={passwordHelperText}
-        />
-        <div className="subform-container">
-          {status && status.non_field_errors && (
-            <div className="non-fields-error">
-              <span>{status.non_field_errors}</span>
-            </div>
-          )}
-          <div className="form-buttons-wrapper">
-            <Button
-              unelevated
-              type="submit"
-              className="material-button"
-              solid={'true'}
-              disabled={isSubmitting}
-            >
-              Submit
-            </Button>
-          </div>
-        </div>
-      </Form>
+function RegistrationForm(props) {
+  const [
+    passwordVisibilityCondition,
+    setPasswordVisibilityCondition,
+  ] = useState(false)
+  const switchPasswordVisibility = () => {
+    setPasswordVisibilityCondition(
+      passwordVisibilityCondition => !passwordVisibilityCondition
     )
   }
+  const {
+    status,
+    isSubmitting,
+    passwordOnChange,
+    onChange,
+    passwordHelperText,
+  } = props
+
+  return (
+    <Form>
+      <h3 className="form-header">Sign Up</h3>
+      <Field
+        id="username"
+        label="Username"
+        name="username"
+        type="text"
+        component={FormikMaterialTextField}
+      />
+      <Field
+        id="email"
+        label="email"
+        name="email"
+        type="email"
+        component={FormikMaterialTextField}
+      />
+      <Field
+        id="password"
+        label="password"
+        name="password1"
+        type={passwordVisibilityCondition ? 'text' : 'password'}
+        tralingIcon={
+          passwordVisibilityCondition ? 'visibility' : 'visibility_off'
+        }
+        tralingIconOnClick={switchPasswordVisibility}
+        component={FormikMaterialTextField}
+        onChange={e => {
+          onChange(e)
+          passwordOnChange(e)
+        }}
+        helperText={passwordHelperText}
+      />
+      <div className="subform-container">
+        {status && status.non_field_errors && (
+          <div className="non-fields-error">
+            <span>{status.non_field_errors}</span>
+          </div>
+        )}
+        <div className="form-buttons-wrapper">
+          <Button
+            unelevated
+            type="submit"
+            className="material-button"
+            solid={'true'}
+            disabled={isSubmitting}
+          >
+            Submit
+          </Button>
+        </div>
+      </div>
+    </Form>
+  )
 }
 
 export default RegistrationForm
