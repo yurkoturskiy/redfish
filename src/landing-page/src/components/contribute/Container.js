7f604f9bc10e16f78ed165e66c27d993d2350389
guandjoy <guandjoy@gmail.com>
2019-11-08 09:13:35 +0200

    Improve accessabilitty

--- src/landing-page/src/components/contribute/Container.js
+++ src/landing-page/src/components/contribute/Container.js
@@ -12,10 +12,10 @@ function ContributeContainer() {
         feel free to participate.
       </p>
       <div className="icons">
-        <a href="https://github.com/guandjoy/redfish" ariaLabel="GitHub">
+        <a href="https://github.com/guandjoy/redfish" aria-label="GitHub">
           <GitHubIcon id="github-icon" className="icon" />
         </a>
-        <a href="https://twitter.com/guandjoy" ariaLabel="Twitter">
+        <a href="https://twitter.com/guandjoy" aria-label="Twitter">
           <TwitterIcon id="twitter-icon" className="icon" />
         </a>
       </div>
