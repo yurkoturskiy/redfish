e664edd8998098c6417fb2cbdee956806d604e7c
guandjoy <guandjoy@gmail.com>
2019-11-12 03:35:31 +0200

    Revert BlobbyCloud

--- src/landing-page/src/components/hero/HeroContainer.js
+++ src/landing-page/src/components/hero/HeroContainer.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 // Local components
 //  * Content
 import Blobs from './content/Blobs'
-import BlobbyCloud from './content/BlobbyCloud'
 import ContentWrapper from './ContentWrapper'
 import VideoDialog from './content/VideoDialog'
 import Header from './content/Header'
@@ -17,7 +16,7 @@ const HeroContainer = () => {
   const [dialog, setDialog] = useState(false)
   return (
     <div className="hero-container">
-      <BlobbyCloud />
+      <Blobs />
       <ContentWrapper>
         <Header />
         {dialog && <VideoDialog setDialog={setDialog} />}
